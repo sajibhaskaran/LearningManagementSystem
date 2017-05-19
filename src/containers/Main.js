078e491787f710e8c39294ae6a748eb811d936ee
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-05-19 10:29:28 -0700

    updated some components

---
+++ src/containers/Main.js
@@ -0,0 +1,43 @@
+﻿// libraries
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+// actions
+
+class Main extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			
+			<StudentDashboard />
+			
+			);
+
+
+	}
+
+
+}
+
+
+
+function mapStateToProps(state) {
+	return {
+		
+	};
+
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({
+		
+	},
+		dispatch
+	);
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserImage);
