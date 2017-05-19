00837f403f86ca2ff19a2175b476651006674507
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-05-19 10:36:57 -0700

    working on a Main component for acting as a landing page

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,11 +2,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
 // actions
+import { profileAccess } from "../../actions/profile_access_action";
 
 class Main extends Component {
 	constructor(props) {
 		super(props);
+	}
+
+
+	componentDidMount() {
+		// calling the action to bring the user info
+		this.props.profileAccess()
 	}
 
 	render() {
@@ -26,18 +34,18 @@ class Main extends Component {
 
 function mapStateToProps(state) {
 	return {
-		
+		userData: state.userData
 	};
 
 }
 
 function mapDispatchToProps(dispatch) {
 	return bindActionCreators({
-		
+		profileAccess
 	},
 		dispatch
 	);
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserImage);
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
