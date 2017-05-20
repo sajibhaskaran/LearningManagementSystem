cb71384ad9fd4697c0b8418346d8f04a9cd2cf34
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-05-19 22:12:41 -0700

    working on a Main component for acting as a landing page

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,1 +1,59 @@
-﻿
+﻿/ libraries
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+// Loader
+import Loader from '../../components/loader/loader';
+
+// actions
+import { resultsInstructorViewAction } from '../../actions/Results/results_instructor_view_action';
+
+
+
+
+class Modal extends Component {
+
+
+
+
+	render() {
+		
+		if (!this.props.show) {
+			
+			return null;
+		}
+
+		return (
+			<div className="container text-center">
+
+				<div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+					<div className="modal-dialog" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<h4 className="modal-title" id="myModalLabel">Modal title</h4>
+							</div>
+							<div className="modal-body">
+								...
+      </div>
+							<div className="modal-footer">
+								<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+								<button type="button" className="btn btn-primary">Save changes</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		);
+
+
+	}
+
+
+}
+
+
+export default Modal;
